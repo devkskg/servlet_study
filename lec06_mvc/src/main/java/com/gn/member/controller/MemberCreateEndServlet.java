17a5b60e08cc7476643f9777a5790713233f3ce1
devkskg <devkskg@gmail.com>
2025-02-13 12:12:51 +0900

    [ksk] lec06_mvc 회원가입 페이지 작성중
    	- 빈값, 비밀번호 확인 틀린 input 있을 경우 alert

--- lec06_mvc/src/main/java/com/gn/member/controller/MemberCreateEndServlet.java
+++ lec06_mvc/src/main/java/com/gn/member/controller/MemberCreateEndServlet.java
@@ -1,32 +1,22 @@
 package com.gn.member.controller;
 
 import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/* 
- * 클래스명 : MemberCreateServlet
- * 주요기능 : 회원가입 화면으로 전환
- * 개발자 : 강성관
- * 개발일자 : 2025-02-13
- * 이런식으로 흔적을 남긴다. 
-*/
-@WebServlet("/memberCreate")
-public class MemberCreateServlet extends HttpServlet {
+@WebServlet("/memberCreateEnd")
+public class MemberCreateEndServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    public MemberCreateServlet() {
+    public MemberCreateEndServlet() {
         super();
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher view = request.getRequestDispatcher("/views/member/create.jsp");
-		view.forward(request, response);
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- lec06_mvc/src/main/webapp/index.jsp
+++ lec06_mvc/src/main/webapp/index.jsp
@@ -11,5 +11,12 @@
 	<!-- 웹컨텍스트패스 안써도 되는 이유는 인덱스 위치가 위치인만큼 -->
 	<%@ include file="/views/include/header.jsp" %>
 	<%@ include file="/views/include/nav.jsp" %>
+	<section>
+		<div id="section_wrap">
+			<div class="word">
+				<h3>HOME</h3>
+			</div>
+		</div>
+	</section>
 </body>
 </html>
